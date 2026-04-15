fd3947872bbd1164cf805ac6ed1b9fb48c7c0340
Samder2025 <shawqhwaisi@gmail.com>
2026-04-15 21:05:47 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 # إعدادات البوت
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "948954784:AAHbcsz-flD6ekzT5i2IN6MorYnoY4EGsPI")
-API_ID = int(os.environ.get("API_ID", 23269382))
+API_ID = "int(os.environ.get("API_ID", 23269382))"
 API_HASH = os.environ.get("API_HASH", "fe19c565fb4378bd5128885428ff8e26")
 
 # مجلدات التخزين
